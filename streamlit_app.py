0089ce6789ec5cc8c3c66a9881d1decd6f1f3173
carolinedlu <42614552+carolinedlu@users.noreply.github.com>
2022-02-09 10:02:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -272,7 +272,7 @@ with col2:
         "demo-deepdream",
     )
     app(
-        "Document Anonymizer",
+        "Anonymization",
         "Detect entities (persons, orgs, and locations) present in a document and anonymize them in one click.",
         "images/DocumentAnonymizer.png",
         "https://share.streamlit.io/charlywargnier/streamlit-text-anonymizer/main/app.py",
